9674d3c3962307e13f3b655df477e0e4be49d5b4
TercioLins <tercio_lins@hotmail.com>
2021-03-30 12:06:00 -0300

    Refatoração da função listarPets

--- index.js
+++ index.js
@@ -11,40 +11,32 @@ const atualizarBanco = () => {
     fs.writeFileSync('bancoDados.json', petsAtualizado, 'utf-8');
 }
 
-const listarPets = () => {
+// const listarPets = () => {
 
-    bancoDados.pets.forEach((pet) => {
+//     bancoDados.pets.forEach((pet) => {
 
-        console.log(`${pet.nome}, ${pet.idade} anos, ${pet.tipo}, ${pet.raca}, ${(pet.vacinado) ? 'vacinado': 'não vacinado'}`);
+//         console.log(`${pet.nome}, ${pet.idade} anos, ${pet.tipo}, ${pet.raca}, ${(pet.vacinado) ? 'vacinado': 'não vacinado'}`);
     
-        pet.servicos.forEach((servico) => {
-            console.log(`${servico.data} - ${servico.nome}`);
-        })
+//         pet.servicos.forEach((servico) => {
+//             console.log(`${servico.data} - ${servico.nome}`);
+//         })
+//     })
+// }
+
+const listarPets = () => {
+    
+    const petsList = bancoDados.pets.filter((pet)=>{
+        let {nome, idade, tipo, raca, vacinado} = pet
+        console.log(`${nome}, ${idade} ano(s) de idade, ${tipo}, ${raca}`);
+
+        for (const servico of pet.servicos) {
+            let{nome, data} = servico
+            console.log(`Serviço: ${nome} | Realizado em ${data}`);
+            console.log(`------------------------`)
+        }
+     
     })
 }
-// const listarPets = () => {
-//     for (let pet of bancoDados.pets) {
-//         //template string
-//         console.log(`${pet.nome}, ${pet.idade}, ${pet.tipo}, ${pet.raca}, ${(pet.vacinado) ? 'vacinado': 'não vacinado'}`);
-
-//         for (const servico of pet.servicos) {
-//             console.log(`${servico.data} - ${servico.nome}`);
-//         }
-//     }
-// }
-
-// const listarPets = () => {
-    
-//     for (let pet of bancoDados.pets){
-//         console.log(`${pet.nome}, ${pet.idade} ${anosDeIdade(pet)} , ${pet.tipo}, raça ${pet.raca}.`);
-
-//         for (const servico of pet.servicos) {
-//             console.log(`Serviço: ${servico.nome} | Realizado em: ${servico.data}`);
-//         }
-//         if (pet.vacinado ? console.log('Está vacinado!') : console.log('Não vacinado!'));
-//         console.log('-----------------------------')
-//     }
-// }
 
 const vacinarPet = pet => {
     if (!pet.vacinado) {
@@ -146,12 +138,12 @@ const filtrarTutor = (nomeTutor) => {
 
 
 
-filtrarTutor('Edson');
-
+//filtrarTutor('Edson');
+//listarPets('luna');
 // darBanhoPet(bancoDados.pets[0]);
 
 // console.log("-----------")
-// listarPets();
+listarPets();
 
 // adicionarPet({
 //     "nome": "Romarinho",
